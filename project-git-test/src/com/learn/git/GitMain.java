c21ee711e29e937dcbeecefe1e0ad6e17cb6e154
lee <1575005415@163.com>
2019-06-12 10:49:30 +0800

    [add] 测试git项目

---
+++ project-git-test/src/com/learn/git/GitMain.java
@@ -0,0 +1,11 @@
+package com.learn.git;
+
+public class GitMain {
+    public static void main(String[] args) {
+        System.out.println("My git test!!!");
+        System.out.println("My git test!!!");
+        System.out.println("My git test!!!");
+        System.out.println("My git test!!!");
+        System.out.println("My git test!!!");
+    }
+}
