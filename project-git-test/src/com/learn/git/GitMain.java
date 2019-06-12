df92a21b6bb89962adc3b8d07163700bd2fc77bf
lee <1575005415@163.com>
2019-06-12 11:16:46 +0800

    [modify] 修改文件

--- project-git-test/src/com/learn/git/GitMain.java
+++ project-git-test/src/com/learn/git/GitMain.java
@@ -7,12 +7,17 @@ public class GitMain {
         System.out.println("My git test!!!");
         System.out.println("My git test!!!");
         System.out.println("My git test!!!");
+        //test test
     }
 
     /**
      * 测试修改提交操作
      */
     public static  void modifyTest(){
+
+        int i=0;
+
+        i+=10;
 
         System.out.println("Modify!!!!");
     }
